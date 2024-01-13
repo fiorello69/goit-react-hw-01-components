ef2683e152457e579f30587807d227c420e1f429
Viorel Breban <vbreban69@gmail.com>
2024-01-13 13:19:12 +0100

    last comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import FriendList from './FriendList/FriendList';
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHystory/transactions.json';
+import TransactionHystory from './TransactionHystory/TransactionHystory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Uploaded statistics" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHystory items={transactions} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+
+function FriendList({ friends }) {
+  return (
+    <ul className="friends-list">
+      {friends.map(friend => (
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          key={friend.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
